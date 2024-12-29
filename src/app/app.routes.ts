24d99e082cb7060d5451b39156bc275a415c752d
Swathi Y <yswathi174@gmail.com>
2024-12-29 22:29:38 +0530

    adminchart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes,RouterModule, provideRouter } from '@angular/router';
 import { FeedbackFormComponent } from './feedback/components/feedback-form/feedback-form.component';
+import { AdminchartComponent } from './feedback/adminComponent/adminchart/adminchart.component';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     {
         path: 'feedback', component:FeedbackFormComponent
+      },
+      {
+        path: 'admin', component:AdminchartComponent
       }
 ];
