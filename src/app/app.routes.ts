bba2ea71cfcf01f7c8f5a95379d0a6c9e2825847
Swathi Y <yswathi174@gmail.com>
2025-01-02 19:27:43 +0530

    sprint1 final

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,20 @@
-
-import { Routes, RouterModule, provideRouter } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { FeedbackFormComponent } from './feedback/components/feedback-form/feedback-form.component';
 import { AdminchartComponent } from './feedback/adminComponent/adminchart/adminchart.component';
-import { AppComponent } from './app.component';
 import { AckPageComponent } from './feedback/components/feedback-form/ack-page/ack-page.component';
 import { PageFeedbackComponent } from './feedback/components/feedback-form/page-feedback/page-feedback.component';
 
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+export const routes: Routes = [
+    { path: 'feedback', component: FeedbackFormComponent },
+    { path: 'admin', component: AdminchartComponent },
+    { path: 'ackpage', component: AckPageComponent },
+    { path: 'reply', component: PageFeedbackComponent }
 
-export const routes: Routes = [];
+];
+
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule],
 })
 export class AppRoutingModule {}
-
-
