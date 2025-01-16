6a6adec8eeb8d8c5ed6671ef320ea9f21de885f2
anniket350 <aniketrai350@gmail.com>
2025-01-16 09:28:56 +0530

    Update app.routes.ts
    Adding redirect page component in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -19,9 +19,8 @@ import { FeedbackFormComponent } from './feedback/components/feedback-form/feedb
 import { AdminchartComponent } from './feedback/components/adminchart/adminchart.component';
 import { AckPageComponent } from './feedback/components/feedback-form/ack-page/ack-page.component';
 import { PageFeedbackComponent } from './feedback/components/feedback-form/page-feedback/page-feedback.component';
-
 import { HomeComponent } from './User profile -authentication/components/home/home.component';
-
+import { RedirectPageComponent } from './feedback/components/redirect-page/redirect-page.component';
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'userDetails', component: UserDetailsComponent },
@@ -37,7 +36,7 @@ export const routes: Routes = [
   { path: 'admin', component: AdminchartComponent },
   { path: 'ackpage', component: AckPageComponent },
   { path: 'reply', component: PageFeedbackComponent },
-  
+  {path: 'redirect', component: RedirectPageComponent},
   {
     path: 'userDetails',
     component: UserDetailsComponent,
